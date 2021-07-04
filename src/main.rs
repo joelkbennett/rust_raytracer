c7dd084a18f627b5a898958bba9a8e3f6a4fe319
Joel Bennett <joelkbennett@gmail.com>
2021-07-03 21:20:57 -0700

    Increase image to FHD

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ fn main() {
 
     // Image
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 1920;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
 
     // Camera
